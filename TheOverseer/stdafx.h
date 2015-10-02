4cd2d8e56b5059365cf512f628993038a6d2bbd5
Sean <seanbrhn3@gmail.com>
2015-10-02 00:23:14 -0400

    Facial recognition

---
+++ TheOverseer/stdafx.h
@@ -0,0 +1,26 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#include "targetver.h"
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <stdio.h>
+#include <tchar.h>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/features2d/features2d.hpp>
+#include <opencv2/objdetect/objdetect.hpp>
+#include <opencv2/videoio/videoio.hpp>
+
+
+
+
+// TODO: reference additional headers your program requires here
